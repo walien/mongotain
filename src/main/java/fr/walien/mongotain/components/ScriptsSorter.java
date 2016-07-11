730fc04a98debb71422ee3e69a73a59b95e48b50
Elian ORIOU <elian.oriou@4sh.fr>
2016-07-11 23:58:30 +0200

    renaming

--- src/main/java/fr/walien/mongotain/components/ScriptsSorter.java
+++ src/main/java/fr/walien/mongotain/components/ScriptsSorter.java
@@ -23,16 +23,16 @@ public class ScriptsSorter {
     }
 
     public static int versionCompare(String str1, String str2) {
-        String[] vals1 = str1.split(SEPARATOR_PATTERN);
-        String[] vals2 = str2.split(SEPARATOR_PATTERN);
+        String[] versions1 = str1.split(SEPARATOR_PATTERN);
+        String[] versions2 = str2.split(SEPARATOR_PATTERN);
         int i = 0;
-        while (i < vals1.length && i < vals2.length && vals1[i].equals(vals2[i])) {
+        while (i < versions1.length && i < versions2.length && versions1[i].equals(versions2[i])) {
             i++;
         }
-        if (i < vals1.length && i < vals2.length) {
-            int diff = Integer.valueOf(vals1[i]).compareTo(Integer.valueOf(vals2[i]));
+        if (i < versions1.length && i < versions2.length) {
+            int diff = Integer.valueOf(versions1[i]).compareTo(Integer.valueOf(versions2[i]));
             return Integer.signum(diff);
         }
-        return Integer.signum(vals1.length - vals2.length);
+        return Integer.signum(versions1.length - versions2.length);
     }
 }
